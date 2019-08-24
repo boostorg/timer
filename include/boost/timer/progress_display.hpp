c221a60102ed618a82116f84407de3f1713aa10f
Peter Dimov <pdimov@gmail.com>
2019-08-24 15:57:15 +0300

    Copy progress_display into boost/timer/progress_display.hpp

--- include/boost/timer/progress_display.hpp
+++ include/boost/timer/progress_display.hpp
@@ -1,81 +1,25 @@
-//  boost progress.hpp header file  ------------------------------------------//
 
-//  Copyright Beman Dawes 1994-99.  Distributed under the Boost
-//  Software License, Version 1.0. (See accompanying file
-//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+// Copyright Beman Dawes 1994-99.
+// Copyright Peter Dimov 2019.
+// Distributed under the Boost Software License, Version 1.0.
+// (http://www.boost.org/LICENSE_1_0.txt)
+//
+// See http://www.boost.org/libs/timer for documentation.
 
-//  See http://www.boost.org/libs/timer for documentation.
+#ifndef BOOST_TIMER_PROGRESS_DISPLAY_HPP_INCLUDED
+#define BOOST_TIMER_PROGRESS_DISPLAY_HPP_INCLUDED
 
-//  Revision History
-//   1 Dec 01  Add leading progress display strings (suggested by Toon Knapen)
-//  20 May 01  Introduce several static_casts<> to eliminate warning messages
-//             (Fixed by Beman, reported by Herve Bronnimann)
-//  12 Jan 01  Change to inline implementation to allow use without library
-//             builds. See docs for more rationale. (Beman Dawes) 
-//  22 Jul 99  Name changed to .hpp
-//  16 Jul 99  Second beta
-//   6 Jul 99  Initial boost version
-
-#ifndef BOOST_PROGRESS_HPP
-#define BOOST_PROGRESS_HPP
-
-#include <boost/config/header_deprecated.hpp>
-BOOST_HEADER_DEPRECATED( "the facilities in <boost/timer/timer.hpp> or <boost/timer/progress_display.hpp>" )
-
-#include <boost/timer.hpp>
 #include <boost/noncopyable.hpp>
-#include <boost/cstdint.hpp>  // for uintmax_t
 #include <iostream>           // for ostream, cout, etc
 #include <string>             // for string
 
 namespace boost {
-
-//  progress_timer  ----------------------------------------------------------//
-
-//  A progress_timer behaves like a timer except that the destructor displays
-//  an elapsed time message at an appropriate place in an appropriate form.
-
-class progress_timer : public timer, private noncopyable
-{
-  
- public:
-  explicit progress_timer( std::ostream & os = std::cout )
-     // os is hint; implementation may ignore, particularly in embedded systems
-     : timer(), noncopyable(), m_os(os) {}
-  ~progress_timer()
-  {
-  //  A) Throwing an exception from a destructor is a Bad Thing.
-  //  B) The progress_timer destructor does output which may throw.
-  //  C) A progress_timer is usually not critical to the application.
-  //  Therefore, wrap the I/O in a try block, catch and ignore all exceptions.
-    try
-    {
-      // use istream instead of ios_base to workaround GNU problem (Greg Chicares)
-      std::istream::fmtflags old_flags = m_os.setf( std::istream::fixed,
-                                                   std::istream::floatfield );
-      std::streamsize old_prec = m_os.precision( 2 );
-      m_os << elapsed() << " s\n" // "s" is System International d'Unites std
-                        << std::endl;
-      m_os.flags( old_flags );
-      m_os.precision( old_prec );
-    }
-
-    catch (...) {} // eat any exceptions
-  } // ~progress_timer
-
- private:
-  std::ostream & m_os;
-};
-
+namespace timer {
 
 //  progress_display  --------------------------------------------------------//
 
 //  progress_display displays an appropriate indication of 
 //  progress at an appropriate place in an appropriate form.
-
-// NOTE: (Jan 12, 2001) Tried to change unsigned long to boost::uintmax_t, but
-// found some compilers couldn't handle the required conversion to double.
-// Reverted to unsigned long until the compilers catch up. 
 
 class progress_display : private noncopyable
 {
@@ -140,6 +84,7 @@ class progress_display : private noncopyable
   } // display_tic
 };
 
+} // namespace timer
 } // namespace boost
 
-#endif  // BOOST_PROGRESS_HPP
+#endif  // BOOST_TIMER_PROGRESS_DISPLAY_HPP_INCLUDED
